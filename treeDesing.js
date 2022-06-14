0968ffbeb0e58311b5566b25b276bb86c9dc82f0
aviash2507 <avinash2632@gmail.com>
2022-06-14 17:48:16 +0530

    number Play

---
+++ treeDesing.js
@@ -0,0 +1,31 @@
+
+for(var a=1; a <10; a++) {
+    for (var b=1; b <=a; b++) {
+        document.write(`${a}`);
+    }    
+    document.write("<br>");
+}
+for(var a=10; a>=1; a--) {
+    for (var b=1; b <=a; b++) {
+        document.write(`${a}`);
+    }    
+    document.write("<br>");
+}
+for(var a=10; a>=1; a--) {
+    for (var b=1; b <=a; b++) {
+        document.write(`${b}`);
+    }    
+    document.write("<br>");
+}
+for(var a=10; a>=1; a--) {
+    for (var b=1; b <=a; b++) {
+        document.write(`${a}`);
+    }    
+    document.write("<br>");
+}
+for(var a=1; a <10; a++) {
+    for (var b=1; b <=a; b++) {
+        document.write(`${a}`);
+    }    
+    document.write("<br>");
+}
