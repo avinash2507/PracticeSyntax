906c81730ac70ac1c7149bfda17f649647dcfa63
aviash2507 <avinash2632@gmail.com>
2022-06-14 17:47:54 +0530

    tree design

---
+++ Practice.js
@@ -0,0 +1,9 @@
+for(var i = 1; i <=10;i++) {
+   
+    console.log(`"===========>Table of ${i}<==========="`)
+   
+    for(var j = 1; j <=10;j++) {
+        document.write("<br>");
+        document.write(`${i} * ${j} = ${i*j}`); 
+    
+}}
